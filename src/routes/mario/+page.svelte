083a67b830f80e3210f8a51a75d0f4e2cab86a71
Vinicius Lemos <lemos.vncs@gmail.com>
2023-02-16 16:56:36 -0300

    mario 0.2

--- src/routes/mario/+page.svelte
+++ src/routes/mario/+page.svelte
@@ -1,84 +1,85 @@
 <script>
+    let emocoes = {}
+    let versao = 0;
+    // let data = [];
+    let frase = "É melhor, muito melhor, contentar-se com a realidade. se ela não é tão brilhante como os sonhos, tem pelo menos a vantagem de existir.";
+    click();
 
-    let emocoes = {
-        version: 0.1, 
-        emoções: {
-            alegria: 0, raiva: 0, nojo: 0, medo: 0, tristeza: 0, surpresa: 0, carga: 0
-        }
-    };
-    let frase = 'Estou com raiva e triste';
     export async function click() {
-        //  
         // const res = await fetch("https://mario-x4fs6sryfq-rj.a.run.app/", {mode: 'no-cors'});
-        let url = "https://mario-x4fs6sryfq-rj.a.run.app/?frase=" + frase
-        console.log(url)
-        const res = await fetch(url, 
-        {
-            mode: 'cors', 
-            headers: {dataType: 'json'},
+        let url = "https://mario-x4fs6sryfq-rj.a.run.app/?frase=" + frase;
+        console.log(url);
+        const res = await fetch(url, {
+            mode: "cors",
+            headers: { dataType: "json" },
         });
         // const item = await res.json();
         // return { item } ;
-        emocoes = await res.json();
-        emocoes = emocoes
-    }   
-    // let emocoes = click();
-    // let emocoes = click()
-    // fetch("https://mario-x4fs6sryfq-rj.a.run.app/", {mode: 'no-cors'})
-    //     .then((response) => {
-    //         return response.json();
-    //     })
-    //     .then((data) => {
-    //     let version = data.version;
-    // })
-    // let result = emocoes
-    // async function fetchMario() {
-    //     let options = {
-    //         method: 'GET',
-    //         mode: 'no-cors',
-    //     }
-    //     const response = await fetch("https://mario-x4fs6sryfq-rj.a.run.app/", options);
-    //     const emocoes = await response.text();
-    //     return emocoes
-    // }
+        let result = await res.json();
 
-    // let emocoes = fetchMario().then(emocoes => {
-    //     emocoes
-    // });
-    
+        versao = result.version;
+        // data = result.data;
+        emocoes = result.mario.emocoes;
+        emocoes = emocoes;
+        // console.log(result)
+    }
 
 </script>
-<div class="container mx-auto">
 
+<div class="container mx-auto">
     <div class="py-12">
         <div class="container mx-auto">
-            <h2 class="my-6 mb-2 text-lg font-semibold text-gray-900 dark:text-white">Emoções</h2>
-        
+            <h2
+                class="my-6 mb-2 text-lg font-semibold text-gray-900 dark:text-white"
+            >
+                Emoções
+            </h2>
+
             <div class="mb-6">
-                <label><input bind:value={frase} placeholder="first" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></label>
-                <button type="button" on:click={click} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> Clique </button>
+                <label
+                    ><input
+                        bind:value={frase}
+                        placeholder="first"
+                        class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    /></label
+                >
+                <button
+                    type="button"
+                    on:click={click}
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                    Clique
+                </button>
             </div>
-        
         </div>
-        
-        
+
         <ul class="max-w-md space-y-1 list-disc list-inside dark:text-gray-400">
-            <li>Alegria: { (emocoes.emoções.alegria * 100).toFixed(2) }%</li>
-            <li>Tristeza: { (emocoes.emoções.tristeza * 100).toFixed(2) }%</li>
-            <li>Medo: { (emocoes.emoções.medo * 100).toFixed(2) }%</li>
-            <li>Nojo: { (emocoes.emoções.nojo * 100).toFixed(2) }%</li>
-            <li>Raiva: { (emocoes.emoções.raiva * 100).toFixed(2) }%</li>
-            <li>Surpresa: { (emocoes.emoções.surpresa * 100).toFixed(2) }%</li>
+            <li>Alegria: {(emocoes.alegria * 100).toFixed(2)}%</li>
+            <li>Tristeza: {(emocoes.tristeza * 100).toFixed(2)}%</li>
+            <li>Medo: {(emocoes.medo * 100).toFixed(2)}%</li>
+            <li>Nojo: {(emocoes.nojo * 100).toFixed(2)}%</li>
+            <li>Raiva: {(emocoes.raiva * 100).toFixed(2)}%</li>
+            <li>Surpresa: {(emocoes.surpresa * 100).toFixed(2)}%</li>
         </ul>
     </div>
 
+    <!-- <p>Carga: { emocoes.emoções.carga }</p> -->
+    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <p><i>Este modelo foi estatisticamente validado.</i></p>
+        <!-- <p></p> -->
+        <p>
+            Esta I.A. foi criado para prever emoções em textos com mais do que
+            10 palavras em frases completas.
+        </p>
 
 
-
-<!-- <p>Carga: { emocoes.emoções.carga }</p> -->
-<div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <p class="py-4">Mario: { emocoes.version } </p>
-    <img class="rounded-full w-36 h-36" src='img/mario.png' alt="Imagem do um homem de bigodes anotando coisas em um caderno e te julgando" width="200" height="200"/>
-</div>
-
+        <p class="py-4">{ versao }</p>
+        <img
+            class="rounded-full w-36 h-36"
+            src="img/mario.png"
+            alt="Imagem do um homem de bigodes anotando coisas em um caderno e te julgando"
+            width="200"
+            height="200"
+        />
+    </div>
 </div>
