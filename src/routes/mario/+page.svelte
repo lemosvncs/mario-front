1d6b5d48cd4f7653819cce9cdf6256d5296b5f6f
Vinicius Lemos <lemos.vncs@gmail.com>
2023-02-23 13:33:21 -0300

    frase de 0 carga, centralizações

--- src/routes/mario/+page.svelte
+++ src/routes/mario/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     let emocoes = {}
     let versao = 0;
+    let carga = 0;
     // let data = [];
     let frase_hl = ' ';
 
@@ -23,51 +24,63 @@
         frase_hl = result.mario.frase; // .replace('/', '');
         frase_hl = frase_hl;
         // data = result.data;
+        
+
+        carga = result.mario.carga;
+        carga = carga;
         emocoes = result.mario.emocoes;
         emocoes = emocoes;
-        console.log(result)
+        // console.log(result)
     }
 
 </script>
 
 
-<div class="container mx-auto">
-    <div class="py-12">
-        <div class="container mx-auto">
-            <h2 class="my-6 mb-2 text-lg font-semibold text-gray-900 dark:text-white">Emoções</h2>
+<div class="container mx-auto px-10">
+    <div class="py-12 mx-auto">
+        <h2 class="my-6 mb-2 text-lg font-semibold text-gray-900 dark:text-white">Emoções</h2>
 
-            <div class="mb-6">
-                <label>
-                    <input
-                        bind:value={frase}
-                        placeholder="first"
-                        class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    /></label
-                >
-                <button
-                    type="button"
-                    on:click={click}
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                >
-                    Clique
-                </button>
-            </div>
+        <div class="mb-6">
+            <label>
+                <input
+                    bind:value={frase}
+                    placeholder="first"
+                    class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                /></label
+            >
+            <button
+                type="button"
+                on:click={click}
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+                Enviar
+            </button>
         </div>
 
-        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">{ @html frase_hl }</div>
+        {#if carga === 0}
+            <p class="text-orange-500">Ops, não consigo determinar as emoções de sua frase. Tente uma frase maior.</p>
+        {/if}
 
-        <ul class="max-w-md space-y-1 list-disc list-inside dark:text-gray-400 p-6 ">
-            <li>Alegria: {(emocoes.alegria * 100).toFixed(2)}%</li>
-            <li>Tristeza: {(emocoes.tristeza * 100).toFixed(2)}%</li>
-            <li>Medo: {(emocoes.medo * 100).toFixed(2)}%</li>
-            <li>Nojo: {(emocoes.nojo * 100).toFixed(2)}%</li>
-            <li>Raiva: {(emocoes.raiva * 100).toFixed(2)}%</li>
-            <li>Surpresa: {(emocoes.surpresa * 100).toFixed(2)}%</li>
-        </ul>
+        {#if carga !== 0}
+            <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm m-auto min-w-full">{ @html frase_hl }</div>
+
+            <div class="">
+                <ul class="max-w-md space-y-1 list-disc list-inside dark:text-gray-400 p-6 m-auto">
+                    <li>Alegria: {(emocoes.alegria * 100).toFixed(2)}%</li>
+                    <li>Tristeza: {(emocoes.tristeza * 100).toFixed(2)}%</li>
+                    <li>Medo: {(emocoes.medo * 100).toFixed(2)}%</li>
+                    <li>Nojo: {(emocoes.nojo * 100).toFixed(2)}%</li>
+                    <li>Raiva: {(emocoes.raiva * 100).toFixed(2)}%</li>
+                    <li>Surpresa: {(emocoes.surpresa * 100).toFixed(2)}%</li>
+                </ul>
+            </div>
+        {/if}
+        
+
     </div>
 
     <!-- <p>Carga: { emocoes.emoções.carga }</p> -->
-    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="block m-auto min-w-full mb-10 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <p><i>Este modelo foi estatisticamente validado.</i></p>
         <!-- <p></p> -->
         <p>
