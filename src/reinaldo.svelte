2b44a0da896f24f37b0ae8b320205b013635eb75
Vinicius Lemos <lemos.vncs@gmail.com>
2023-03-14 14:43:25 -0300

    back to the top

--- src/reinaldo.svelte
+++ src/reinaldo.svelte
@@ -155,8 +155,6 @@
         return p;
     }
 
-
-
     function handleClick() {
         idade               = idade              ;
         sexo                = sexo               ;
@@ -181,10 +179,11 @@
     let inputstyle = "mr-4 mb-2 dark:border-white dark:border-2 dark:bg-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block              p-2.5 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 </script>
 
-<div class="text-center text-center animate-bounce">
-    <Icon icon={keyboard}/>
-</div>
-
+<a id="top">
+    <div class="text-center text-center animate-bounce">
+        <Icon icon={keyboard}/>
+    </div>
+</a>
 <div class="mx-auto text-center">
     <p>Probabilidade de você <b>NÃO</b> ser diagnosticado com hipertensão:</p>
     <h2 class="text-2xl">{reinaldo.toFixed(2)*100}%</h2>
@@ -315,11 +314,15 @@
                 <p>Não</p>
             </label>
         </div>
-        <button type="button"
-            on:click={handleClick}
-            class="w-64 h-16 mb-16 mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-            <Icon icon={paperPlane}></Icon>
-                <!-- <i class="fa-solid fa-user text-white"></i> -->
-        </button>
+        <a href="#top">
+            <button type="button"
+                id='botaoEnviar'
+                on:click={handleClick}
+                class="w-64 h-16 mb-16 mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                <Icon icon={paperPlane}></Icon>
+                    <!-- <i class="fa-solid fa-user text-white"></i> -->
+            </button>
+        </a>
     </form>
+    
 </div>
