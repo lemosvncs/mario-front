8cc80e34f9b027c361e186f5a7f3fde4a30076a3
Vinicius Lemos <lemos.vncs@gmail.com>
2023-03-27 15:02:50 -0300

    luide

---
+++ src/routes/luide/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Header from "../../header.svelte";
+</script>
+<div class="dark bg-black">
+    <Header/>
+    <iframe class="w-full" width="600" height="800" src="https://lookerstudio.google.com/embed/reporting/320faa37-bbda-41c1-ab2c-95729a3464d1/page/BwBKD" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
